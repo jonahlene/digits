073e79bd72eeae6f61877e6399ed4fe796f4586c
Jonah Lene <jonah20@hawaii.edu>
2023-10-31 22:35:23 -1000

    contacts collection finished in 26:52 minutes.

--- app/imports/ui/pages/ListContactsAdmin.jsx
+++ app/imports/ui/pages/ListContactsAdmin.jsx
@@ -4,16 +4,16 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Contacts } from '../../api/contact/Contacts';
-import Contact from '../components/Contact';
+import ContactAdmin from '../components/ContactAdmin';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-const ListContacts = () => {
+const ListContactsAdmin = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, contacts } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Contacts.userPublicationName);
+    const subscription = Meteor.subscribe(Contacts.adminPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
@@ -29,12 +29,12 @@ const ListContacts = () => {
       <Row className="justify-content-center">
         <Col>
           <Col className="text-center">
-            <h2 style={{ color: 'white' }}>List Contacts</h2>
+            <h2 style={{ color: 'white' }}>List Contacts (Admin)</h2>
           </Col>
           <Row xs={1} md={2} lg={3} className="g-4">
             {contacts.map((contact) => (
               <Col key={contact._id}>
-                <Contact contact={contact} />
+                <ContactAdmin contact={contact} />
               </Col>
             ))}
           </Row>
@@ -44,4 +44,4 @@ const ListContacts = () => {
   ) : <LoadingSpinner />);
 };
 
-export default ListContacts;
+export default ListContactsAdmin;
